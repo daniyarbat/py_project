1f6847e69a0c22c8e167c7bf3ecc8e2098ea8b36
Daniyar Batyrbaev <batyrbaev_daniyar@mail.ru>
2023-08-07 12:42:15 +0600

    Импорт функций в main.py

---
+++ main.py
@@ -0,0 +1,7 @@
+from funcs import add, subtract
+
+result_add = add(5, 3)
+print("Результат сложения:", result_add)
+
+result_subtract = subtract(10, 7)
+print("Результат вычитания:", result_subtract)
